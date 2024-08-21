fb18352182da4d5ee6b46a31be7cf5d469e70856
MarcinhoLetsCode <darlan_lifecat@yahoo.com.br>
2024-08-21 15:23:19 -0300

    introducao datetime

---
+++ objetosDate_objetosDatetime_objetosTime.py
@@ -0,0 +1,10 @@
+import datetime
+d = datetime.date(2024, 8, 20)
+print(d)
+print(str(d.day)+"-"+str(d.month)+"-"+str(d.year))
+
+from datetime import date
+data = datetime.date(2024, 8, 20)
+print(data)
+
+print(str(data.day)+"-"+str(data.month)+"-"+str(data.year))
