551fc8a7c9c696803e27bcc909703d5808761890
MarcinhoLetsCode <darlan_lifecat@yahoo.com.br>
2024-08-21 15:37:13 -0300

    introducao datetime final

--- objetosDate_objetosDatetime_objetosTime.py
+++ objetosDate_objetosDatetime_objetosTime.py
@@ -1,10 +1,22 @@
-import datetime
-d = datetime.date(2024, 8, 20)
-print(d)
-print(str(d.day)+"-"+str(d.month)+"-"+str(d.year))
+#import datetime
+#d = datetime.date(2024, 8, 20)
+#print(d)
+#print(str(d.day)+"-"+str(d.month)+"-"+str(d.year))
+#print(d.today())
+#print(datetime.datetime.today())
 
-from datetime import date
-data = datetime.date(2024, 8, 20)
+from datetime import date, datetime, time
+#data = datetime.date(2024, 8, 20)
+data = date(2024, 8, 20)
 print(data)
-
 print(str(data.day)+"-"+str(data.month)+"-"+str(data.year))
+print(date.today())
+
+print("")
+data_hora = datetime(2024, 8, 21, 10, 30, 20)
+print(data_hora)
+print(datetime.today())
+
+print("")
+hora = time(10, 20, 0)
+print(hora)
